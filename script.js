cb520b58de4175188f463d03297202da3a0660f0
Liam Tait <142124471+TubaSpinach@users.noreply.github.com>
2023-12-16 16:12:29 -0500

    feat: add calculation functions

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ let numKeys = document.querySelector(".numbers");
 let opsKeys = document.querySelector(".operations");
 let buttArray = []
 let opsArray = []
-
+let output = document.querySelector(".output");
+let little_output = document.querySelector(".little_output");
+let currentEquation = {number1: null, number2: null, op: ""};
 /*create num buttons*/
 for(let i=0; i<12; i++){
     buttArray.push(document.createElement('button'));
@@ -29,11 +31,13 @@ for(let i=0; i<12; i++){
 let operationSymbols = {plus: "+",minus: "-",multiply:"*",divide:"/",equals:"="};
 for(op in operationSymbols){
     let currentButt = document.createElement('button');
+    let currentOp = operationSymbols[op]
     opsArray.push(currentButt);
-
+    console.log(op)
+    console.log(operationSymbols[op])
     currentButt.textContent = operationSymbols[op];
     currentButt.addEventListener('click',() => {
-        op_to_output(operationSymbols[op]);
+        op_to_output(currentOp);
     }
     );
 
@@ -41,3 +45,41 @@ for(op in operationSymbols){
     
 };
 
+function add_to_output(input){
+    output.textContent += input;
+}
+
+function op_to_output(input){
+    if(currentEquation.number1 === null){
+        currentEquation.number1 = parseInt(output.textContent);
+    } else {
+        currentEquation.number2 = parseInt(output.textContent);
+    };
+
+    little_output.innerHTML += output.textContent + " " + input + "<br>";
+    output.textContent = "";
+
+    if(input !== "="){
+        currentEquation.op = input;
+        
+    } else {
+        little_output.innerHTML += evaluate_output() + "<br>";
+    };
+}
+
+function evaluate_output(){
+    switch(currentEquation.op) {
+        case "+":
+            return currentEquation.number1 + currentEquation.number2;
+        
+        case "-":
+            return currentEquation.number1 - currentEquation.number2;
+        
+        case "*":
+            return currentEquation.number1*currentEquation.number2;
+        
+        case "/":
+            return currentEquation.number1/currentEquation.number2;
+    };
+     
+}
